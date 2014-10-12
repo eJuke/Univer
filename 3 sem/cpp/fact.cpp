e126209f732006b2d02485e2b3cee97cb5166708
eJuke <evgeniy.juke@gmail.com>
2014-10-12 23:49:38 +0400

    Kolpak fact

--- 3 sem/cpp/fact.cpp
+++ 3 sem/cpp/fact.cpp
@@ -1,37 +1,62 @@
 #include <iostream>
-#include <vector>
 #include <cstdlib>
 using namespace std;
 
-struct hello{
+struct test {
 	int digit;
 	int module;
 };
 
 class fact{
 public:
-	void start(){
-		cout << "Enter the number that u want to fact: ";
-		cin >> size;
-		answer();
+	void start(int size){
+		for(int i = 1; i < 3000; ++i){
+			hello[i].digit = -1;
+			hello[i].module = 0;
+		}
+		hello[0].digit = 1;
+		hello[0].module = 0;
+		answer(size);
+		printFact();
 	}
-private:
-	vector <hello> result;
-	hello pp;
-	pp.digit = 1;
-	pp.module = 0;
-	int size;
-	result.push_back(pp);
-	void answer(){
-		for (int now = 2; now < size + 1; ++now){
+	void answer(int size){
+		int count, countMod;
+		for(int i = 2; i < size+1; ++i){
+			count = 0;
+			for (int j = 0; hello[j].digit != -1; j++){
+				hello[j].digit *= i;
+				hello[j].module = hello[j].digit / 10;
+				hello[j].digit %= 10;
+				count = j;
+			}
 
+			for (int k = 0; k < count+1; ++k){
+				countMod = k+1;
+				while (hello[k].module != 0){
+					if (hello[k].digit > 9){
+						hello[k].module += hello[k].digit / 10;
+						hello[k].digit %= 10;
+					}
+					if (hello[countMod].digit == -1) hello[countMod].digit++;
+					hello[countMod].digit += hello[k].module % 10;
+					hello[k].module /= 10;
+					countMod++;
+				}
+			}
 		}
 	}
+	void printFact(){
+		int countPr;
+		for (int i = 0; hello[i].digit != -1; ++i) countPr = i;
+		for (countPr; countPr >= 0; --countPr) cout << hello[countPr].digit;
+	}
+protected:
+	test hello[3000];
 };
 
 int main(int argc, char const *argv[])
 {
 	fact Fact;
-	Fact.start();
+	Fact.start(atoi(argv[1]));
 	return 0;
 }
